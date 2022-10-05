f952ea601699f034bdced6f44d8abf46ec2f54a0
StanWang <StanWang@jquan.com.tw>
2022-10-05 13:37:35 +0800

    [Add] 1.前端增加共用方法 2.安裝NOPI套件

---
+++ MvcProjectTemplate/Scripts/share/cookie.js
@@ -0,0 +1,59 @@
+﻿/**
+* Set Cookie
+* @param {*} obj 
+*/
+function setCookie(obj) {
+          var { name, value, day } = obj;
+
+          var cookie = name + "=" + encodeURIComponent(value);
+
+          if (typeof day === "number") {
+                    cookie += `; max-age= + ${ day * 24 * 60 * 60 } ;SameSite=strict`;
+          }
+
+          document.cookie = cookie;
+}
+
+/**
+ * Get Cookie
+ * @param {*} cname 
+ * @returns 
+ */
+function getCookie(cname) {
+          let name = cname + "=";
+          let decodedCookie = decodeURIComponent(document.cookie);
+          let ca = decodedCookie.split(";");
+          for (let i = 0; i < ca.length; i++) {
+                    let c = ca[i];
+                    while (c.charAt(0) == " ") {
+                              c = c.substring(1);
+                    }
+                    if (c.indexOf(name) == 0) {
+                              return c.substring(name.length, c.length);
+                    }
+          }
+          return "";
+}
+
+/**
+ * Delete Cookie
+ * @param {*} name 
+ */
+function deleteCookie(name) {
+          document.cookie = ` ${ name }= ''; max-age= 0;`;
+}
+
+//Useage
+//設定Cookie
+// var obj = {
+//   name:'user',
+//   value:'stan',
+//   day:1
+// }
+// setCookie(obj)
+
+//取得Cookie
+// console.log("user: " + getCookie('user'))
+
+//刪除Cookie
+// deleteCookie('user');
---
+++ MvcProjectTemplate/Scripts/share/localstorage.js
@@ -0,0 +1,31 @@
+﻿/**
+ * setLocalStorage
+ * @param {any} obj
+ */
+function setLocalStorage(obj) {
+          var { key, value } = obj;
+          localStorage.setItem(key, value);
+}
+
+/**
+ * getLocalStorage
+ * @param {any} key
+ */
+function getLocalStorage(key) {
+          return localStorage.getItem(key);
+}
+
+/**
+ * removeLocalStorage
+ * @param {any} key
+ */
+function removeLocalStorage(key) {
+          return localStorage.removeItem(key);
+}
+
+/**
+ * clearAllLocalStorage
+ * */
+function clearAllLocalStorage() {
+          localStorage.clear();
+}
