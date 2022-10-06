293925e7361a32f2f7786698a8a8140441bba030
StanWang <StanWang@jquan.com.tw>
2022-10-06 11:10:12 +0800

    [Add] Add SqlScripts Example

---
+++ MvcProjectTemplate/SqlScripts/Book.sql
@@ -0,0 +1,14 @@
+﻿
+CREATE TABLE `book` (
+  `Id` int NOT NULL AUTO_INCREMENT COMMENT 'PK',
+  `ISBN` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'ISBN',
+  `Title` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'Title',
+  `Description` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT 'Description',
+  `CreateTime` datetime DEFAULT NULL COMMENT 'CreateTime',
+  `UpdateTime` datetime DEFAULT NULL COMMENT 'UpdateTime',
+  `Author` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT 'Author',
+  `CategoryId` int NOT NULL COMMENT 'Category Id',
+  PRIMARY KEY (`Id`),
+  KEY `CategoryId` (`CategoryId`),
+  CONSTRAINT `book_ibfk_1` FOREIGN KEY (`CategoryId`) REFERENCES `category` (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='書';
---
+++ MvcProjectTemplate/SqlScripts/Category.sql
@@ -0,0 +1,11 @@
+﻿ 
+ CREATE TABLE `category` (
+  `Id` int NOT NULL AUTO_INCREMENT COMMENT 'PK',
+  `Name` varchar(100) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'Category Name',
+  `DisplayOrder` int DEFAULT NULL COMMENT '分類排序',
+  `CreateTime` datetime NOT NULL COMMENT 'CreateTime',
+  `UpdateTime` datetime DEFAULT NULL COMMENT 'UpdateTime',
+  `CreateUser` int NOT NULL COMMENT 'User Id',
+  `UpdateUser` int DEFAULT NULL COMMENT 'User Id',
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='分類';
